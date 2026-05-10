03e3463633e3447b38b31e36f9d911aa0d337d9b
Sofia Runsten <runsten.sofia@gmail.com>
2026-05-10 16:54:12 +0200

    added color

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,12 @@ import Box from '@mui/material/Box';
 const Header = () => {
   return (
     <Box sx={{
-      textAlign: "center", py: 3,
+      textAlign: "center",
+      py: 3,
       px: 2,
       borderRadius: 2,
-      // bgcolor: 'blue', 
-      backdropFilter: 'blur(10px)',
-      border: '1px solid black',
+      color: "white",
+      border: '1px solid white',
       mb: 4
     }}>
       <h1>SHOPPING LIST</h1>
